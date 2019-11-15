9a955884b22254dc863531c2ce36a855f76ff78e
yusuketakamatsu <sleepingforest03@gmail.com>
2019-11-15 09:43:53 +0900

    add components.

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+import Link from 'next/Link';
+
+const linkStyle = {
+  marginRight: 15
+};
+
+const Header = () => (
+  <div>
+    <Link href="/">
+      <a style={linkStyle}>Home</a>
+    </Link>
+    <Link href="/about">
+      <a style={linkStyle}>About</a>
+    </Link>
+  </div>
+);
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
-import Link from 'next/Link';
+import Header from '../components/Header';
 
-const Index = () => (
-  <div>
-    <Link href="/about">
-      <a title="About Page">About Page</a>
-    </Link>
-    <p>Hello Next.js</p>
-  </div>
-);
-
-export default Index;
+export default function Index() {
+  return (
+    <div>
+      <Header />
+      <p>Hello Next,js</p>
+    </div>
+  );
+}
